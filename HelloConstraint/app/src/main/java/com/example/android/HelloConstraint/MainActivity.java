70612d86d05e086c4b9178cb487b09d191fe481f
108590030 <t108590030@ntut.edu.tw>
2021-03-08 22:52:09 +0800

    HW2

---
+++ HelloConstraint/app/src/main/java/com/example/android/HelloConstraint/MainActivity.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.HelloConstraint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Displays two Buttons and a TextView.
+ * - Pressing the TOAST button shows a Toast.
+ * - Pressing the COUNT button increases the displayed mCount.
+ *
+ * Note: Fixing behavior when device is rotated is a challenge exercise for the student.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    private Button bCount, bZero;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        bCount = (Button) findViewById(R.id.button_count);
+        bZero = (Button) findViewById(R.id.button_zero);
+    }
+
+    /*
+    * Shows a Toast when the TOAST button is clicked.
+    *
+    * @param view The view that triggered this onClick handler.
+    *             Since a toast always shows on the top,
+    *             the passed in view is not used.
+    */
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    /*
+    * Increments the number in the TextView when the COUNT button is clicked.
+    *
+    * @param view The view that triggered this onClick handler.
+    *             Since the count always appears in the TextView,
+    *             the passed in view is not used.
+    */
+    public void countUp(View view) {
+        mCount++;
+        bZero.setBackgroundColor(0xFF3F51B5);
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        if (mCount%2==1){
+            bCount.setBackgroundColor(0xFF7F00FF);
+        }
+        if (mCount%2==0){
+            bCount.setBackgroundColor(0xFFBC8F8F);
+        }
+    }
+
+    public void toZero(View view){
+        mCount = 0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        bZero.setBackgroundColor(0xFF808080);
+        bCount.setBackgroundColor(0xFF3F51B5);
+    }
+}
